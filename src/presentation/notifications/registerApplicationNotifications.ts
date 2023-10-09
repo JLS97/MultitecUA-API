5886346b98cf4554132087fdf758b7f3b058ffd5
Jaime López <23194952+JLS97@users.noreply.github.com>
2023-10-09 18:38:47 +0200

    Update registerApplicationNotifications.ts

--- src/presentation/notifications/registerApplicationNotifications.ts
+++ src/presentation/notifications/registerApplicationNotifications.ts
@@ -1,5 +1,7 @@
+import { registerAnnouncementsNotifications } from "./announcements/registerAnnouncementsNotifications";
 import { registerUserNotifications } from "./users/registerUserNotifications";
 
 export function registerApplicationNotifications() {
   registerUserNotifications();
+  registerAnnouncementsNotifications();
 }
